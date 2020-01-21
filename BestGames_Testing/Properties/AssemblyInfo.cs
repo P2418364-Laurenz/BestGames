4af08891aa8d0cccef244d635fde3681cccfef26
Laurenz Guevara <P2418364@LEC-ADMIN.dmu.ac.uk>
2020-01-21 15:42:38 +0000

    Add project files.

---
+++ BestGames_Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BestGames_Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BestGames_Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee7c8d87-cbb3-44e3-8403-ce86dd2d8792")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BestGames_Testing/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BestGames_Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+    }
+}
