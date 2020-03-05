3b25c6cf05d4fb7f52cdacfac6f0d1fbbb5805ea
Lewis Harris <P2419279@my365.dmu.ac.uk>
2020-03-05 19:34:07 +0000

    Added password encryption method "encryptPass()" - edits the clsCustomer cusPassword variable

--- BestGames_Libary/clsCustomer.cs
+++ BestGames_Libary/clsCustomer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BestGames_Libary
 {
@@ -42,6 +43,26 @@ namespace BestGames_Libary
                 "\", email:\"" + this.cusEmail + "\", password:\"" +
                 this.cusPassword + "\", dateRegistered:\"" + this.cusDateRegister +
                 "\", status:\"" + this.cusAccountStatus + "\"}";
+        }
+
+        public void estEncrypt()
+        {
+
+        }
+
+        public void encryptPass()
+        {
+            //Check if password has already been converted to md5, if true DO NOT hash again!
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(this.cusPassword);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+            }
         }
 
     }
